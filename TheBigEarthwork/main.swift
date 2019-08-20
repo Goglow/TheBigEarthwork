5dd34a109d969f300b4726c6bfd65246aee489d2
Goglow <waggle.glow@gmail.com>
2019-08-19 22:41:00 -0400

    Initial Commit

---
+++ TheBigEarthwork/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TheBigEarthwork
+//
+//  Created by Melissa GS on 19/08/2019.
+//  Copyright © 2019 Goglow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
