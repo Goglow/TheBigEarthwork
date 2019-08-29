360b28c9fa994b1695f58fea776e0af1ffbda5e4
Goglow <waggle.glow@gmail.com>
2019-08-29 05:20:17 -0400

    Add bow.swift

---
+++ TheBigEarthwork/bow.swift
@@ -0,0 +1,18 @@
+//
+//  bow.swift
+//  TheBigEarthwork
+//
+//  Created by Waggle Glow on 29/08/2019.
+//  Copyright © 2019 Goglow. All rights reserved.
+//
+
+import Foundation
+
+// Chest : malus
+// Damage + Repair
+
+class Bow: Arm {
+    init() {
+        super.init(damage: 5, repair: 5)
+    }
+}
