c7b4ec1271a80614a58ec485ac3b4bd73d6a7aaa
Goglow <waggle.glow@gmail.com>
2019-08-29 06:03:17 -0400

    Add malus files

---
+++ TheBigEarthwork/Chest/moonstone.swift
@@ -0,0 +1,18 @@
+//
+//  moonstone.swift
+//  TheBigEarthwork
+//
+//  Created by Waggle Glow on 29/08/2019.
+//  Copyright © 2019 Goglow. All rights reserved.
+//
+
+import Foundation
+
+// Chest: malus
+// Repair
+
+class Moonstone: Arm {
+    init() {
+        super.init(damage: 0, repair: 10)
+    }
+}
