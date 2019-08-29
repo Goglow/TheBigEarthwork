1e8a641d8f188a0dfef87d73d69b2d5f7d2fc848
Goglow <waggle.glow@gmail.com>
2019-08-29 05:19:12 -0400

    Add boomerang.swift

---
+++ TheBigEarthwork/boomerang.swift
@@ -0,0 +1,19 @@
+//
+//  boomerang.swift
+//  TheBigEarthwork
+//
+//  Created by Waggle Glow on 29/08/2019.
+//  Copyright © 2019 Goglow. All rights reserved.
+//
+
+import Foundation
+
+
+// Chest : bonus
+// Damage + Repair
+
+class Boomerang: Arm {
+    init() {
+        super.init(damage: 25, repair: 25)
+    }
+}
