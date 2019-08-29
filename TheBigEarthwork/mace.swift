735d951fb4cf83d5d7ab3a9e2e6b642656366fb0
Goglow <waggle.glow@gmail.com>
2019-08-29 05:24:00 -0400

    Add chest files

---
+++ TheBigEarthwork/mace.swift
@@ -0,0 +1,18 @@
+//
+//  mace.swift
+//  TheBigEarthwork
+//
+//  Created by Waggle Glow on 29/08/2019.
+//  Copyright © 2019 Goglow. All rights reserved.
+//
+
+import Foundation
+
+// Chest : bonus
+// Damage
+
+class Mace: Arm {
+    init() {
+        super.init(damage: 50, repair: 0)
+    }
+}
---
+++ TheBigEarthwork/slingshot.swift
@@ -0,0 +1,18 @@
+//
+//  slingshot.swift
+//  TheBigEarthwork
+//
+//  Created by Waggle Glow on 29/08/2019.
+//  Copyright © 2019 Goglow. All rights reserved.
+//
+
+import Foundation
+
+// Chest : malus
+// Damage
+
+class Slingshot: Arm {
+    init() {
+        super.init(damage: 10, repair: 0)
+    }
+}
