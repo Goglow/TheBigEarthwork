c96cbb8e517de9a348caefe3fee237d9f14a0476
Goglow <waggle.glow@gmail.com>
2019-10-14 05:50:58 -0400

    Update chest file

--- TheBigEarthwork/chest.swift
+++ TheBigEarthwork/chest.swift
@@ -14,14 +14,14 @@ struct Chest {
     func chestRandom() -> Weapon? {
         let round = Int(arc4random_uniform(100))
         if round >= 70 {
-            print("\nGood job ! You have found a chest !")
+            print("\nGood job! You have found a chest!")
             return chestWeapon()
         }
         return nil
     }
     // A weapon is taken at random in the chest.
     private func chestWeapon() -> Weapon {
-        print("In this chest, a weapon appears randomly and it is given to your character !")
+        print("In this chest, a weapon appears randomly and it is given to your character!")
         let randomIndex = Int(arc4random_uniform(UInt32(chest.count)))
         return chest[randomIndex]
     }
